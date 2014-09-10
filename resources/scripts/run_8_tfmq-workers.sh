dbc85821632c5a57115bda44821402dcb12f3e56
jjcorreao <JoaquinCorrea@lbl.gov>
2014-09-10 00:36:03 -0700

    pbsgen path conflicts

--- resources/scripts/run_8_tfmq-workers.sh
+++ resources/scripts/run_8_tfmq-workers.sh
@@ -11,18 +11,34 @@
 #
 #export PATH=$PATH:$(pwd)
 
-export OMERO_HOME = /project/projectdirs/ngbi/omero5/OMERO.server
-
-for i in {1..8}
-do
-    # tfmq-worker -b 3 -t 30 -q tfmqtaskqueue &
-#    /project/projectdirs/ngbi/jobs/tfmq/tfmq-worker
-    ${OMERO_HOME}/lib/scripts/OMERO.ImageJ/resources/taskfarmermq/tfmq-worker -b 3 -t 10 &
-    #sleep 2
-done
-wait
+#export OMERO_HOME = /project/projectdirs/ngbi/omero5/OMERO.server
+#
+#for i in {1..8}
+#do
+#    # tfmq-worker -b 3 -t 30 -q tfmqtaskqueue &
+##    /project/projectdirs/ngbi/jobs/tfmq/tfmq-worker
+#    ${OMERO_HOME}/lib/scripts/OMERO.ImageJ/resources/taskfarmermq/tfmq-worker -b 3 -t 10 &
+#    #sleep 2
+#done
+#wait
 
 ##
 ## To start the client, 
 ## tfmq-client -i taskEnv.lst -w 0 -r 1 -q tfmqtaskqueue
 ##
+
+module load python_base
+export VENV=/project/projectdirs/ngbi/jobs/tfmq/python_test
+export TFMQ_PATH=/project/projectdirs/ngbi/jobs/tfmq
+
+source /project/projectdirs/ngbi/jobs/tfmq/python_test/bin/activate
+
+export PATH=$PATH:$(pwd)
+
+for i in {1..8}
+do
+    # tfmq-worker -b 3 -t 30 -q tfmqtaskqueue &
+    /project/projectdirs/ngbi/jobs/tfmq/tfmq-worker -b 3 -t 10 &
+    #sleep 2
+done
+wait
